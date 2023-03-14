17b893a787a004b076e05d5bbe6eede24e37a48d
seungyeoni <dnri33@naver.com>
2023-03-14 11:30:51 +0900

    C:/Program Files/Git/home/main2, /home/main3, /home/increase

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // 아래 있는 HomeController 는 컨트롤러이다.
 @Controller
 public class HomeController {
+    public int i=-1;
     // @GetMapping("/home/main") 의 의미
     // 개발자가 스프링부트에게 말한다.
     // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
@@ -18,5 +19,24 @@ public class HomeController {
     @ResponseBody
     public String showMain() {
         return "안녕하세요!!";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2() {
+        return "반갑습니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3() {
+        return "즐거웠습니다.";
+    }
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int increase() {
+        i++;
+        return i;
     }
 }
